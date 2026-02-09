6df72d94a13f0bfd51a8008159c95ddc8030802c
Shae Erisson <shae+jj@scannedinavian.com>
2026-02-09 16:47:05 -0500

    memmap2 example builds and runs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use std::fs::File;
+use std::io::Read;
+
+use memmap2::Mmap;
+use thiserror::Error;
+
+fn main() -> Result<(), MmapError> {
+    let mut file = File::open("README.org")?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+    let mmap = unsafe { Mmap::map(&file)? };
+    assert_eq!(&contents[..], &mmap[..]);
     println!("Hello, world!");
+    Ok(())
+}
+
+#[derive(Error,Debug)]
+pub enum MmapError {
+    #[error("io unhappy")]
+    IoError(#[source] #[from] std::io::Error),
+
 }
