ff71ed85a1a85d89b6a4dde3e9d33bea71ea890f
Shae Erisson <shae+jj@scannedinavian.com>
2026-02-09 20:23:52 -0500

    clippy lint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::fs::File;
 use std::{fs::OpenOptions, io::Read};
-use std::path::PathBuf;
-use tempfile;
 
-use memmap2::{Mmap, MmapMut, MmapOptions};
+use memmap2::{Mmap, MmapOptions};
 use thiserror::Error;
 
 fn main() -> Result<(), MmapError> {
@@ -14,7 +12,8 @@ fn main() -> Result<(), MmapError> {
     let temp_dir = tempfile::tempdir()?;
     let out_path = temp_dir.path().join("outfile");
     // for whatever reason, must have read even if only writing!
-    let out_file = OpenOptions::new().write(true).read(true).create(true).open(out_path)?;
+    let out_file = OpenOptions::new().write(true).read(true).create(true).truncate(true).open(out_path)?;
+    // set the write size to the read buffer size, or crash and burn
     out_file.set_len(mmap_in.len() as u64)?;
     // println!("gonna write to {:?}",out_file);
     let mut mmap_out = unsafe { MmapOptions::new().map_mut(&out_file)? };
